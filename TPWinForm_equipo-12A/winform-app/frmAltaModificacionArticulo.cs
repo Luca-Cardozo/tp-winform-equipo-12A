1586631dd3956cb8955cc5457ad528289260c835
Luca Cardozo <lucacardozo99@gmail.com>
2026-04-15 19:54:26 -0300

    Creación de clases de dominio, negocio y presentación y AccesoDatos

---
+++ TPWinForm_equipo-12A/winform-app/frmAltaModificacionArticulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+
+namespace winform_app
+{
+    public partial class frmAltaModificacionArticulo : Form
+    {
+        public frmAltaModificacionArticulo()
+        {
+            InitializeComponent();
+        }
+    }
+}
